2582e9993afdc9629e61fd602146ce6748b2ed66
Zebra <Thirawatp9@gmail.com>
2022-04-26 12:29:07 +0700

    Delete en.py

--- Deploy Model API (FastAPI) on DigitalOcean/en.py
+++
@@ -1,9 +0,0 @@
-import socket
-import random
-
-serverip = "104.248.39.146"
-port = 27617
-
-server = socket.socket()
-server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-server.connect((serverip, port))
